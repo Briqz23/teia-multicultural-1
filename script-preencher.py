1b998f7250a7e26359cc00c2c3ff4a5ef36308da
Briqz23 <dbriquez.devmaua@gmail.com>
2023-06-19 23:09:02 -0300

    mudança de requisito :x

--- script-preencher.py
+++ script-preencher.py
@@ -1,4 +1,5 @@
 import io
+import textwrap
 from urllib.request import urlopen
 from PIL import Image, ImageFont, ImageDraw
 from tkinter import messagebox
@@ -39,9 +40,13 @@ def criar():
     o_que = o_que_text.get("1.0", "end-1c")
     pra_que = pra_que_text.get("1.0", "end-1c")
     como = como_text.get("1.0", "end-1c")
-    strings = [numero_atv, etapa, materia, turma, quinzenario, sequencia, conteudo, o_que, pra_que, como]
+    instrucoes = instrucoes_text.get("1.0", "end")
+    wrapped_instrucoes = textwrap.wrap(instrucoes, width=50)
+    lines = instrucoes.split('\n')
+
+    strings = [numero_atv, etapa, materia, turma, quinzenario, sequencia, conteudo, o_que, pra_que, como, instrucoes]
     
-    strings_upper = [s.upper() for s in strings]
+    
     if etapa == "Definir":
         img = Image.open("templates/DEFINIR-1.png")
     elif etapa == "Descobrir":
@@ -54,22 +59,34 @@ def criar():
     draw = ImageDraw.Draw(img)
 
     #numero_atv
-    draw.text((1154, 225), strings_upper[0], font=firstFont, fill='black')
+    draw.text((1154, 225), strings[0], font=firstFont, fill='black')
     #matéria
     W = 1700
-    _, _, w, h = draw.textbbox((0, 130), strings_upper[2], font=materiaFont)
-    draw.text(((W-w)/2, 130), strings_upper[2], font=materiaFont, fill='white')
+    _, _, w, h = draw.textbbox((0, 130), strings[2], font=materiaFont)
+    draw.text(((W-w)/2, 130), strings[2], font=materiaFont, fill='white')
 
     #turma
-    draw.text((320, 410), strings_upper[3], font=firstFont, fill='black')
+    draw.text((320, 410), strings[3], font=firstFont, fill='black')
     #quinzenário
-    draw.text((980, 418), strings_upper[4], font=firstFont, fill='black')
+    draw.text((980, 418), strings[4], font=firstFont, fill='black')
     #sequencia
-    draw.text((1310, 418), strings_upper[5], font=firstFont, fill='black')
+    draw.text((1310, 418), strings[5], font=firstFont, fill='black')
     #conteúdo
-    draw.text((750, 510), strings_upper[6], font=firstFont, fill='black')
+    draw.text((750, 510), strings[6], font=firstFont, fill='black')
+    #instrucoes
+    #draw.text((332, 1063), strings[10], font=firstFont, fill='black')
+    y=1363
+    for line in lines:
+        wrapped_lines = textwrap.wrap(line, width=100) # Adjust the width as per your requirements
 
+        for wrapped_line in wrapped_lines:
+            draw.text((332, y), wrapped_line, font=firstFont, fill='black')
+            y += firstFont.getbbox(wrapped_line)[1] + 35
+
+    y += 35  # Add additional spacing between paragraphs if needed
     messagebox.showinfo("Sucesso", "Arquivo criado com sucesso! Eles serão armazenados na pasta ABRIR > fichas")
+    
+
 
     #limitar a 150 caracteres no front
     def quebrar_string(string, length):
@@ -90,7 +107,7 @@ def criar():
         return lista
 
 
-    lista_o_que = quebrar_string(strings_upper[7],60)
+    lista_o_que = quebrar_string(strings[7],60)
     o_que_y = 820
     for i in range((len(lista_o_que))):
     
@@ -98,7 +115,7 @@ def criar():
         o_que_y+=35
 
 
-    lista_pra_que = quebrar_string(strings_upper[8],60)
+    lista_pra_que = quebrar_string(strings[8],60)
     pra_que_y = 980
     for i in range((len(lista_pra_que))):
     
@@ -106,10 +123,10 @@ def criar():
         pra_que_y+=35
 
 
-    lista_como = quebrar_string(strings_upper[9],60)
+    lista_como = quebrar_string(strings[9],60)
     como_y = 1120
     for i in range((len(lista_como))):
-    
+        
         draw.text((350, como_y), lista_como[i], font=firstFont, fill='black')
         como_y+=35
 
@@ -139,7 +156,7 @@ def criar():
 
     current_date = datetime.now().strftime("%d_%m_%Y")
     output_folder = "ABRIR/fichas"
-    file_name = f"FICHA-{strings_upper[1]}-{datetime.now().strftime('%Y-%m-%d')}.pdf"
+    file_name = f"FICHA-{strings[1]}-{datetime.now().strftime('%Y-%m-%d')}.pdf"
     output_path = os.path.join(output_folder, file_name)
 
     # Save the PDF file
@@ -173,32 +190,32 @@ etapa_combobox.grid(row=0, column=1, padx=5, pady=5)
 
 numero_atividade_label = ttk.Label(user_info_frame, text="Número de Atividade", font=("Arial", 12, "bold"))
 numero_atividade_label.grid(row=0, column=2, padx=5, pady=5)
-numero_atividade_entry = ttk.Entry(user_info_frame)
+numero_atividade_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 numero_atividade_entry.grid(row=0, column=3, padx=5, pady=5)
 
 materia_label = ttk.Label(user_info_frame, text="Matéria", font=("Arial", 12, "bold"))
 materia_label.grid(row=1, column=0, padx=5, pady=5)
-materia_entry = ttk.Entry(user_info_frame)
+materia_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 materia_entry.grid(row=1, column=1, padx=5, pady=5)
 
 turma_label = ttk.Label(user_info_frame, text="Turma/Ano", font=("Arial", 12, "bold"))
 turma_label.grid(row=1, column=2, padx=5, pady=5)
-turma_entry = ttk.Entry(user_info_frame)
+turma_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 turma_entry.grid(row=1, column=3, padx=5, pady=5)
 
 sequencia_label = ttk.Label(user_info_frame, text="Sequência", font=("Arial", 12, "bold"))
 sequencia_label.grid(row=2, column=0, padx=5, pady=5)
-sequencia_entry = ttk.Entry(user_info_frame)
+sequencia_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 sequencia_entry.grid(row=2, column=1, padx=5, pady=5)
 
 conteudo_label = ttk.Label(user_info_frame, text="Conteúdo", font=("Arial", 12, "bold"))
 conteudo_label.grid(row=2, column=2, padx=5, pady=5)
-conteudo_entry = ttk.Entry(user_info_frame)
+conteudo_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 conteudo_entry.grid(row=2, column=3, padx=5, pady=5)
 
 quinzenario_label = ttk.Label(user_info_frame, text="Quinzenário", font=("Arial", 12, "bold"))
 quinzenario_label.grid(row=3, column=0, padx=5, pady=5)
-quinzenario_entry = ttk.Entry(user_info_frame)
+quinzenario_entry = ttk.Entry(user_info_frame, font=("Arial", 10))
 quinzenario_entry.grid(row=3, column=1, padx=5, pady=5)
 
 # Caixas de texto
@@ -208,21 +225,26 @@ text_frame.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
 o_que_label = ttk.Label(text_frame, text="O QUE:", font=("Arial", 12, "bold"))
 o_que_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-o_que_text = tk.Text(text_frame, height=2, bg="white")
+o_que_text = tk.Text(text_frame, height=2, bg="white", font=("Arial", 10), wrap='word')
 o_que_text.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
 
 pra_que_label = ttk.Label(text_frame, text="PRA QUÊ?", font=("Arial", 12, "bold"))
 pra_que_label.grid(row=2, column=0, padx=5, pady=5, sticky="w")
 
-pra_que_text = tk.Text(text_frame, height=2, bg="white")
+pra_que_text = tk.Text(text_frame, height=2, bg="white", wrap = 'word', font=("Arial", 10))
 pra_que_text.grid(row=3, column=0, padx=5, pady=5, sticky="nsew")
 
 como_label = ttk.Label(text_frame, text="COMO?", font=("Arial", 12, "bold"))
 como_label.grid(row=4, column=0, padx=5, pady=5, sticky="w")
 
-como_text = tk.Text(text_frame, height=2, bg="white")
+como_text = tk.Text(text_frame, height=2, bg="white", wrap = 'word', font=("Arial", 10))
 como_text.grid(row=5, column=0, padx=5, pady=5, sticky="nsew")
 
+instrucoes_label = ttk.Label(text_frame, text="INTRUÇÕES", font=("Arial", 12, "bold"))
+instrucoes_label.grid(row=6, column=0, padx=5, pady=5, sticky="w")
+
+instrucoes_text = tk.Text(text_frame, height=8, bg="white", wrap = 'word', font=("Arial", 10))
+instrucoes_text.grid(row=7, column=0, padx=5, pady=5, sticky="nsew")
 
 button_frame = tk.Frame(window)
 button_frame.pack(pady=10)
